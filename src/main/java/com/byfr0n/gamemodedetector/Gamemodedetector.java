55af2f39bdc28292074e8c52d5557fad750d64c7
Byfr0n <Byfr0n@users.noreply.github.com>
2025-03-02 15:50:00 +0100

    Initial commit

---
+++ src/main/java/com/byfr0n/gamemodedetector/Gamemodedetector.java
@@ -0,0 +1,10 @@
+package com.byfr0n.gamemodedetector;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gamemodedetector implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/byfr0n/gamemodedetector/utils/PlayerUtils.java
@@ -0,0 +1,4 @@
+package com.byfr0n.gamemodedetector.utils;
+
+public class PlayerUtils {
+}
