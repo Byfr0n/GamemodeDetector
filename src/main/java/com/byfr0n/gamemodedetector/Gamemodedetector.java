0ee714c4431e0d1c3b00d11614fec5c73f47f8a3
Byfr0n <Byfr0n@users.noreply.github.com>
2025-03-09 15:34:42 +0100

    whops pushed wrong code

--- src/main/java/com/byfr0n/gamemodedetector/Gamemodedetector.java
+++ src/main/java/com/byfr0n/gamemodedetector/Gamemodedetector.java
@@ -8,17 +8,25 @@ import com.byfr0n.gamemodedetector.utils.SoundUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class Gamemodedetector implements ClientModInitializer {
     public static final Config CONFIG = new Config();
     public static final Logger LOGGER = LoggerFactory.getLogger("Gamemode Detector");
-    private static GameMode lastGamemode = null;
+
+    private static final Map<UUID, GameMode> lastGamemodes = new HashMap<>();
+
     @Override
     public void onInitializeClient() {
-
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             NotificationRenderer.render(drawContext);
         });
@@ -26,17 +34,20 @@ public class Gamemodedetector implements ClientModInitializer {
         SoundUtils.registerSounds();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                GameMode currentGamemode = PlayerUtils.getPlayerGamemode(client);
+            if (client.world == null || client.getNetworkHandler() == null) return;
 
-                if (currentGamemode != lastGamemode) {
-                    if (currentGamemode != null) {
-                        SoundUtils.playGamemodeSound();
+            for (PlayerEntity player : client.world.getPlayers()) {
+                if (player == client.player) continue;
 
-                        new ChatNotification("You switched gamemodes!").send();
-                        NotificationRenderer.addNotification("Gamemode changed!");
-                    }
-                    lastGamemode = currentGamemode;
+                GameMode currentGamemode = PlayerUtils.getPlayerGamemode(player);
+                UUID playerId = player.getUuid();
+
+                if (currentGamemode != null && currentGamemode != lastGamemodes.get(playerId)) {
+                    SoundUtils.playGamemodeSound();
+                    new ChatNotification(player.getName().getString() + " switched to " + currentGamemode.getName()).send();
+                    NotificationRenderer.addNotification(player.getName().getString() + " changed gamemode!");
+
+                    lastGamemodes.put(playerId, currentGamemode);
                 }
             }
         });
--- src/main/java/com/byfr0n/gamemodedetector/utils/PlayerUtils.java
+++ src/main/java/com/byfr0n/gamemodedetector/utils/PlayerUtils.java
@@ -1,13 +1,21 @@
 package com.byfr0n.gamemodedetector.utils;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.GameMode;
 
 public class PlayerUtils {
-    public static GameMode getPlayerGamemode(MinecraftClient client) {
-        if (client.interactionManager != null) {
-            return client.interactionManager.getCurrentGameMode();
+    public static GameMode getPlayerGamemode(PlayerEntity player) {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        if (client.getNetworkHandler() != null) {
+            PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(player.getUuid());
+            if (entry != null) {
+                return entry.getGameMode();
+            }
         }
-        return null;
+
+        return player.isSpectator() ? GameMode.SPECTATOR : null;
     }
 }
